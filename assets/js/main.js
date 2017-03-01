1505b2ca42694ee5d9674ab8876753f196cecb5c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-03-01 11:43:38 +0100

    Add tawk

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,3 +164,13 @@
 	});
 
 })(jQuery);
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/588a1488bab87e66427b8dfd/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
