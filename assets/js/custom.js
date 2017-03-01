3844a9baeee91a11f236b825d202cdac50ea4caa
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-03-01 13:20:21 +0100

    Add google analytics

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,11 @@ $(".scrollTo").on('click', function(e) {
      scrollTop: ($(target).offset().top)
    }, 700);
 });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72344405-3', 'auto');
+  ga('send', 'pageview');
