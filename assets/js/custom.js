085344fdd1e44108ea0b1a0d56f79c241b4914cb
Hemang Kumar <hemangsk@gmail.com>
2017-03-02 17:57:58 +0530

    Simple Localisation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,38 @@
 /* Smooth Scroll */
 $(".scrollTo").on('click', function(e) {
-   e.preventDefault();
-   var target = $(this).attr('href');
-   $('html, body').animate({
-     scrollTop: ($(target).offset().top)
-   }, 700);
+	e.preventDefault();
+	var target = $(this).attr('href');
+	$('html, body').animate({
+		scrollTop: ($(target).offset().top)
+	}, 700);
+});
+
+$('#selectLanguageDropdown').localizationTool({
+	'defaultLanguage' : 'en_GB',
+	'showFlag': true,
+	'showCountry': true,
+	'showLanguage': true, 
+	'strings' : {
+		'Get Your MVP Done' : {
+			'de_DE' : 'German translation here'
+		},
+		'class:introduction-text' :{
+			'de_DE' : 'German Introduction Text'
+		}
+	}
+});
+
+$.getJSON("http://freegeoip.net/json?callback=?", function(data) {
+	var country_code = data.country_code;
+	var country = data.country_name;
+	var ip = data.ip;
+	var time_zone = data.time_zone;
+	var latitude = data.latitude;
+	var longitude = data.longitude;
+
+	if (country == 'Germany'){
+		$('#selectLanguageDropdown').localizationTool('translate', 'de_DE');
+	}
 });
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
